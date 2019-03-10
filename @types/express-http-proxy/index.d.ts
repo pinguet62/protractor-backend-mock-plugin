56336e602557bd1d6f1fccc7f6d9ef8b0c0a15bb
PINGUET Julien <pinguet62@gmail.com>
2019-03-10 19:20:07 +0100

    Version upgrade

--- @types/express-http-proxy/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "express-http-proxy" {
-
-    import * as express from "express";
-
-    function ExpressHttpProxy(host: string): express.RequestHandler;
-
-    export = ExpressHttpProxy;
-
-}
